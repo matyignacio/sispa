65c4a220687e2ebad0d7f21bcbb3185d3d3d9948
matyignacio <matyignacio11@gmail.com>
2018-04-27 08:46:07 -0300

    Agrego: ConexionDB Adquisicion BienPatrimonial Categoria Estado CodigoPresupuestario Inciso Mueble MuebleMatenible Inmueble Marca Modelo Operaciones Perfil Usuario Reparticion TipoAdquisicion Y modifico tambien ObjetoBase agregandole sus relaciones

---
+++ src/controlador/MuebleControlador.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import objeto.Mueble;
+
+/**
+ *
+ * @author Kuky
+ */
+public class MuebleControlador {
+
+    private Mueble mueble;
+    private ArrayList<Mueble> muebles = new ArrayList<>();
+    private Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    public Mueble extraer(Integer id) throws SQLException {
+        conn = ConexionDB.GetConnection();
+        String consultaSql = "SELECT * FROM SISPA.Muebles WHERE id=?";
+        ps = conn.prepareStatement(consultaSql);
+        ps.setInt(1, id);
+        ps.executeQuery();
+        rs = ps.getResultSet();
+        while (rs.next()) {
+            mueble = new Mueble();
+            mueble.setId(rs.getInt(1));
+            mueble.setNombre(rs.getString(2));
+            mueble.setVisible(rs.getBoolean(3));
+            mueble.setExpediente(rs.getString(4));
+            mueble.setObservaciones(rs.getString(5));
+            //FALTAN CATEGORIA, ESTADO, MODELO, USUARIO Y REPARTICION
+        }
+        rs.close();
+        ps.close();
+        conn.close();
+        return mueble;
+    }
+
+}
