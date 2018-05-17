fb74480625479b999fa4d5995891a1e26f9a80a4
matyignacio <matyignacio11@gmail.com>
2018-05-17 08:39:22 -0300

    Arreglo controladores

--- src/controlador/UsuarioControlador.java
+++ src/controlador/UsuarioControlador.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import objeto.Usuario;
 
-
 public class UsuarioControlador {
 
     private Usuario usuario;
@@ -24,6 +23,7 @@ public class UsuarioControlador {
     private PerfilControlador perfilcontrolador;
 
     public Usuario extraer(Integer id) throws SQLException {
+        perfilcontrolador = new PerfilControlador(); //HAY QUE INICIALIZARLO PARA PODER USARLO
         conn = ConexionDB.GetConnection();
         String consultaSql = "SELECT * FROM public.\"Usuarios\" where id=?";
         ps = conn.prepareStatement(consultaSql);
@@ -46,6 +46,7 @@ public class UsuarioControlador {
     }
 
     public ArrayList<Usuario> extraerTodos() throws SQLException {
+        perfilcontrolador = new PerfilControlador();
         conn = ConexionDB.GetConnection();
         String consultaSql = "SELECT * FROM public.\"Usuarios\" order by id";
         ps = conn.prepareStatement(consultaSql);
@@ -59,6 +60,13 @@ public class UsuarioControlador {
             usuario.setMail(rs.getString(3));
             usuario.setClave(rs.getString(4));
             usuario.setVisible(rs.getBoolean(5));
+            usuario.setPerfil(perfilcontrolador.extraer(rs.getInt(6)));
+            if (rs.getInt(7) != 0) {
+                Usuario usuarioSuperior = new Usuario();
+                UsuarioControlador usuarioControlador = new UsuarioControlador();
+                usuarioSuperior = usuarioControlador.extraer(rs.getInt(7));
+                usuario.setUsuario(usuarioSuperior);
+            }
             usuarios.add(usuario);
         }
         rs.close();
@@ -66,8 +74,9 @@ public class UsuarioControlador {
         conn.close();
         return usuarios;
     }
-    
+
     public ArrayList<Usuario> extraerTodosVisibles() throws SQLException {
+        perfilcontrolador = new PerfilControlador();
         conn = ConexionDB.GetConnection();
         String consultaSql = "SELECT * FROM public.\"Usuarios\" where visible = TRUE order by id";
         ps = conn.prepareStatement(consultaSql);
@@ -81,6 +90,7 @@ public class UsuarioControlador {
             usuario.setMail(rs.getString(3));
             usuario.setClave(rs.getString(4));
             usuario.setVisible(rs.getBoolean(5));
+            usuario.setPerfil(perfilcontrolador.extraer(rs.getInt(6)));
             usuarios.add(usuario);
         }
         rs.close();
@@ -90,54 +100,59 @@ public class UsuarioControlador {
     }
 
     public void insertar(Usuario usuario) throws SQLException {
-        
-         if (JOptionPane.showConfirmDialog(null, "¿Esta seguro que desea guardar?", "ATENCION!", JOptionPane.YES_NO_OPTION) == 0){
-        conn = ConexionDB.GetConnection();
-        String consultaSql = "INSERT INTO public.\"Usuarios\" ( nombre, mail, clave) VALUES (?, ?, ?)";
-        ps = conn.prepareStatement(consultaSql);
-        ps.setString(1, usuario.getNombre());
-        ps.setString(2, usuario.getMail());
-        ps.setString(3, usuario.getClave());
-        ps.execute();
-        JOptionPane.showMessageDialog(null, "Insertado correctamente");
-        ps.close();
-        conn.close();
-         }
+
+        if (JOptionPane.showConfirmDialog(null, "¿Esta seguro que desea guardar?", "ATENCION!", JOptionPane.YES_NO_OPTION) == 0) {
+            conn = ConexionDB.GetConnection();
+            String consultaSql = "INSERT INTO public.\"Usuarios\" ( nombre, mail, clave, visible, id_perfil, id_usuario) VALUES (?, ?, ?, ?, ?, ?)";
+            ps = conn.prepareStatement(consultaSql);
+            ps.setString(1, usuario.getNombre());
+            ps.setString(2, usuario.getMail());
+            ps.setString(3, usuario.getClave());
+            ps.setBoolean(4, usuario.isVisible());
+            ps.setInt(5, usuario.getPerfil().getId());
+            ps.setInt(6, usuario.getUsuario().getId());
+            ps.execute();
+            JOptionPane.showMessageDialog(null, "Insertado correctamente");
+            ps.close();
+            conn.close();
+        }
     }
 
     public void modificar(Usuario usuario) throws SQLException {
-          if (JOptionPane.showConfirmDialog(null, "¿Esta seguro que desea modificar?", "ATENCION!", JOptionPane.YES_NO_OPTION) == 0) {
-        conn = ConexionDB.GetConnection();
-        String consultaSql = "UPDATE public.\"Usuarios\" SET nombre=?, mail=?, clave=?, visible=? WHERE id=?";
-        ps = conn.prepareStatement(consultaSql);
-        ps.setString(1, usuario.getNombre());
-        ps.setString(2, usuario.getMail());
-        ps.setString(3, usuario.getClave());
-        ps.setBoolean(4, usuario.isVisible());
-        ps.setInt(5, usuario.getId());
-        ps.executeUpdate();
-        JOptionPane.showMessageDialog(null, usuario.toString() + " modificado correctamente");
-        ps.close();
-        conn.close();
-          }
+        if (JOptionPane.showConfirmDialog(null, "¿Esta seguro que desea modificar?", "ATENCION!", JOptionPane.YES_NO_OPTION) == 0) {
+            conn = ConexionDB.GetConnection();
+            String consultaSql = "UPDATE public.\"Usuarios\" SET nombre=?, mail=?, clave=?, visible=?, id_perfil=?, id_usuario=? WHERE id=?";
+            ps = conn.prepareStatement(consultaSql);
+            ps.setString(1, usuario.getNombre());
+            ps.setString(2, usuario.getMail());
+            ps.setString(3, usuario.getClave());
+            ps.setBoolean(4, usuario.isVisible());
+            ps.setInt(5, usuario.getPerfil().getId());
+            ps.setInt(6, usuario.getUsuario().getId());
+            ps.setInt(7, usuario.getId());
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, usuario.toString() + " modificado correctamente");
+            ps.close();
+            conn.close();
+        }
     }
 
     public void borrar(Usuario usuario) throws SQLException {
         if (JOptionPane.showConfirmDialog(null, "¿Esta seguro que desea eliminar?", "ATENCION!", JOptionPane.YES_NO_OPTION) == 0) {
-        conn = ConexionDB.GetConnection();
-        String consultaSql = "DELETE FROM public.\"Usuarios\" WHERE id=?";
-        ps = conn.prepareStatement(consultaSql);
-        ps.setInt(1, usuario.getId());
-        ps.executeUpdate();
-        JOptionPane.showMessageDialog(null, usuario.toString() + " eliminado correctamente");
-        ps.close();
-        conn.close();
+            conn = ConexionDB.GetConnection();
+            String consultaSql = "DELETE FROM public.\"Usuarios\" WHERE id=?";
+            ps = conn.prepareStatement(consultaSql);
+            ps.setInt(1, usuario.getId());
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, usuario.toString() + " eliminado correctamente");
+            ps.close();
+            conn.close();
         }
     }
 
     public Usuario validarUsuario(Usuario usuario) throws SQLException {
         conn = ConexionDB.GetConnection();
-        String consultaSql = "select * from public.\"Usuarios\" where mail like ? AND clave like ?";
+        String consultaSql = "select * from public.\"Usuarios\" where mail like ? AND clave like ? AND visible = TRUE";
         ps = conn.prepareStatement(consultaSql);
         ps.setString(1, usuario.getMail());
         ps.setString(2, usuario.getClave());
@@ -152,6 +167,5 @@ public class UsuarioControlador {
         }
         return usuario;
     }
-    
-    
+
 }
