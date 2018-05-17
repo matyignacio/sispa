f776dbe229bcfe43668a651156d729bbcfc28fcd
matyignacio <matyignacio11@gmail.com>
2018-05-17 09:39:12 -0300

    Arreglo herencia adquisicion

---
+++ src/objeto/Adquisicion_Inmueble.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objeto;
+
+/**
+ *
+ * @author Kuky
+ */
+public class Adquisicion_Inmueble extends Adquisicion {
+
+    private Inmueble inmueble;
+
+    public Inmueble getInmueble() {
+        return inmueble;
+    }
+
+    public void setInmueble(Inmueble inmueble) {
+        this.inmueble = inmueble;
+    }
+
+}
