e8777c2478a1f39e4104026b711538b28dedb20f
Kuky <Kuky@KukyNote>
2018-09-24 09:59:50 -0300

    Agrego constantes a Util

--- src/util/Util.java
+++ src/util/Util.java
@@ -13,7 +13,11 @@ public class Util {
 
     public static final int MUEBLE_MANTENIBLE = 1;
     public static final int MUEBLE_NO_MANTENIBLE = 0;
+    public static final int ABM_AJUSTES = 3;
+    public static final int ABM_USUARIOS = 4;
+    public static final int ABM_INFORMES = 5;
     public static final String CORREO = "sispaunlar@gmail.com";
     public static final String CLAVE = "sispasispa";
+    public static final String LOOK_AND_FEEL = "Windows";
 
 }
